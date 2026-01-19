6c314695074213f73433b62b0fb0a051b83f01b1
Jeffryes Chapman <2847218+chapman178@users.noreply.github.com>
2026-01-19 16:02:43 -0500

    added lane cols

--- PWtool_GSC.py
+++ PWtool_GSC.py
@@ -33,10 +33,19 @@ df, file_sha = load_github_data()
 
 
 st.subheader(" Lanes ")
-enable_L1 = st.checkbox ("Lane1_EN", value= True)
-enable_L2 = st.checkbox ("Lane2_EN", value= True)
-enable_L3 = st.checkbox ("Lane3_EN", value= True)
-enable_L4 = st.checkbox ("Lane4_EN", value= True)
+col1, col2, col3 = st.columns([1, 1, 1])
+with col1:
+    enable_L1 = st.checkbox ("Lane1_EN", value= True)
+    enable_L2 = st.checkbox ("Lane2_EN", value= True)
+    enable_L3 = st.checkbox ("Lane3_EN", value= True)
+    enable_L4 = st.checkbox ("Lane4_EN", value= True)
+    st.rerun()
+with col2:
+    pass
+with col3:
+    pass
+
+
 
 st.divider()
 # 3. Display the Data
