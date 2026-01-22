9c36da03cae91e2d7fabdcc1ad1d6cf935551bf7
Jeffryes Chapman <2847218+chapman178@users.noreply.github.com>
2026-01-22 06:32:59 -0500

    still messing with spacing

--- PWtool_GSC.py
+++ PWtool_GSC.py
@@ -176,7 +176,7 @@ with lane_btn2:
 
 
 
-lca1, lcb1, lcc1 = st.columns([1,5,1], gap = 'xxsmall' ,vertical_alignment = 'center')
+lca1, lcb1, lcc1, bog1 = st.columns([1,1,1,5], gap = 'xxsmall' ,vertical_alignment = 'center')
 with lca1:
     st.checkbox ("L1_EN", key = "Ln1_EN")
 with lcb1:
@@ -184,7 +184,7 @@ with lcb1:
 with lcc1:
     st.checkbox ("Ln1_loss", key='Loss_L1')
 
-lca2, lcb2, lcc2 = st.columns([1,5,1], gap = 'small' ,vertical_alignment = 'center')
+lca2, lcb2, lcc2, bog2 = st.columns([1,1,1,5], gap = 'small' ,vertical_alignment = 'center')
 with lca2:
     st.checkbox ("L2_EN", key = "Ln2_EN")
 with lcb2:
@@ -192,7 +192,7 @@ with lcb2:
 with lcc2:
     st.checkbox ("Ln2_loss", key='Loss_L2') 
 
-lca3, lcb3, lcc3 = st.columns([1,5,1], gap = 'small' ,vertical_alignment = 'center')
+lca3, lcb3, lcc3, bog3 = st.columns([1,1,1,5], gap = 'small' ,vertical_alignment = 'center')
 with lca3:
     st.checkbox ("L3_EN", key = "Ln3_EN")
 with lcb3:
@@ -200,7 +200,7 @@ with lcb3:
 with lcc3:
     st.checkbox ("Ln3_loss", key='Loss_L3')
 
-lca4, lcb4, lcc4 = st.columns([1,5,1], gap = 'small' ,vertical_alignment = 'center')
+lca4, lcb4, lcc4, bog4 = st.columns([1,1,1,5], gap = 'small' ,vertical_alignment = 'center')
 with lca4:
     st.checkbox ("L4_EN", key = "Ln4_EN")
 with lcb4:
